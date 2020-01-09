3bd8e75c8223b1d4dd711370759a0310be622efe
Martinez Vera <660974@cognizant.com>
2020-01-09 10:19:13 +0100

    Initial commit

---
+++ src/main/java/com/example/demo/JamExamplesApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JamExamplesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JamExamplesApplication.class, args);
+	}
+
+}
